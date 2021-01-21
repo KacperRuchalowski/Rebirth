43cdf089f90dea1c678fb0abfced29fd6c367b6e
Kacper Ruchałowski <kacperruch@gmail.com>
2021-01-21 21:57:23 +0100

    Project is now more method oriented

--- game_file.py
+++ game_file.py
@@ -42,8 +42,129 @@ class PlayerCharacter:
         "madness": 0,
     }
 
-    def lose_health(self):
-        self.health = 0
+    def move(self, torch):
+        global result, keep, card, active_player
+        self.is_attacked = 0
+        self.has_drawn = 0
+        if torch == 0:
+            result = random.randint(1, 6)
+        elif torch > 0:
+            result = torch
+        if self.active_field[1] == 0 and self.active_field[0] == 0:  # pole 0,0
+            self.active_field[1] += result
+        elif self.active_field[1] in range(1, 8) and self.active_field[0] == 0:  # górny wiersz
+            self.active_field[1] += result
+            if self.active_field[1] > 8 and self.active_field[0] == 0:
+                keep = self.active_field[1] - 8
+                self.active_field[1] = 8
+                self.active_field[0] += keep
+        elif self.active_field[0] == 0 and self.active_field[1] == 8:  # pole 0,8
+            self.active_field[0] += result
+        elif self.active_field[0] in range(1, 8) and self.active_field[1] == 8:  # prawa kolumna
+            self.active_field[0] += result
+            if self.active_field[0] > 8 and self.active_field[1] == 8:
+                keep = self.active_field[0] - 8
+                self.active_field[0] = 8
+                self.active_field[1] -= keep
+        elif self.active_field[0] == 8 and self.active_field[1] == 8:  # pole 8,8
+            self.active_field[1] -= result
+        elif self.active_field[1] in range(1, 8) and self.active_field[0] == 8:  # dolny wiersz
+            self.active_field[1] -= result
+            if self.active_field[1] < 0 and self.active_field[0] == 8:
+                keep = self.active_field[1] * -1
+                self.active_field[1] = 0
+                self.active_field[0] = 8
+                self.active_field[0] -= keep
+        elif self.active_field[0] == 8 and self.active_field[1] == 0:  # pole 8,0
+            self.active_field[0] -= result
+        elif self.active_field[0] in range(1, 8) and self.active_field[1] == 0:
+            self.active_field[0] -= result
+            if self.active_field[0] < 0 and self.active_field[1] == 0:
+                keep = self.active_field[0] * -1
+                self.active_field[0] = 0
+                self.active_field[1] = 0
+                self.active_field[1] += keep
+        chance = random.randint(1, 100)
+        if chance <= 20:
+            self.is_attacked = 1
+        card = None
+        active_player = active_player * -1
+
+    def loseHealth(self):
+        self.health -= 1
+        if self.health <= 0:
+            self.active_field = [0, 0]
+            self.health = 3
+            self.has_torch = 0
+            for item in self.keys:
+                if self.keys[item] == 1:
+                    self.keys[item] = 0
+                    break
+
+    def drawCard(self, other_player):
+        self.has_drawn = 1
+        global Deck
+        random.shuffle(Deck)
+        global card
+        if Deck:
+            card = Deck.pop()
+        else:
+            Deck = cards.build_deck()
+            card = Deck.pop()
+        if card.name == 'KeyBone':
+            self.keys["bone"] = 1
+        elif card.name == 'KeyDeath':
+            self.keys["death"] = 1
+        elif card.name == 'KeyToxic':
+            self.keys["toxic"] = 1
+        elif card.name == 'KeyMadness':
+            self.keys["madness"] = 1
+        elif card.name == 'loseHealth':
+            self.loseHealth()
+        elif card.name == 'magicArrow':
+            other_player.loseHealth()
+        elif card.name == 'recoverHealth':
+            self.health += 1
+        elif card.name == "torch":
+            self.has_torch = 1
+        elif card.name == 'thief':
+            for item in other_player.keys:
+                if other_player.keys[item] == 1:
+                    other_player.keys[item] = 0
+                    break
+        elif card.name == 'loseItem':
+            for item in self.keys:
+                if self.keys[item] == 1:
+                    self.keys[item] = 0
+                    break
+
+    def battle(self):
+        attack_power = random.randint(1, 6) + self.strength
+        enemy_attack = random.randint(1, 6) - self.resistance
+        if attack_power > enemy_attack:
+            check = random.randint(1, 2)
+            if check == 1:
+                self.resistance += 1
+            else:
+                self.strength += 1
+        else:
+            self.loseHealth()
+        self.is_attacked = 0
+
+    def battleBoss(self, bossID):
+        attack_power = random.randint(1, 6) + self.strength
+        boss_attack = random.randint(1, 10) - self.resistance
+        if attack_power > boss_attack:
+            if bossID == 'death':
+                player1.souls['death'] = 1
+            elif bossID == 'bone':
+                player1.souls['bone'] = 1
+            elif bossID == 'toxic':
+                player1.souls['toxic'] = 1
+            elif bossID == 'madness':
+                player1.souls['madness'] = 1
+        else:
+            self.loseHealth()
 
 
 @app.route('/')
@@ -67,224 +188,35 @@ def createCharacter():
 @app.route('/move/<player>/<int:torchAmount>')
 def move(player, torchAmount):
     global result, keep, card, active_player
-
     if player == '1':
-        player1.is_attacked = 0
-        player1.has_drawn = 0
-        if torchAmount == 0:
-            result = random.randint(1, 6)
-        elif torchAmount > 0:
-            result = torchAmount
-        if player1.active_field[1] == 0 and player1.active_field[0] == 0:  # pole 0,0
-            player1.active_field[1] += result
-        elif player1.active_field[1] in range(1, 8) and player1.active_field[0] == 0:  # górny wiersz
-            player1.active_field[1] += result
-            if player1.active_field[1] > 8 and player1.active_field[0] == 0:
-                keep = player1.active_field[1] - 8
-                player1.active_field[1] = 8
-                player1.active_field[0] += keep
-        elif player1.active_field[0] == 0 and player1.active_field[1] == 8:  # pole 0,8
-            player1.active_field[0] += result
-        elif player1.active_field[0] in range(1, 8) and player1.active_field[1] == 8:  # prawa kolumna
-            player1.active_field[0] += result
-            if player1.active_field[0] > 8 and player1.active_field[1] == 8:
-                keep = player1.active_field[0] - 8
-                player1.active_field[0] = 8
-                player1.active_field[1] -= keep
-        elif player1.active_field[0] == 8 and player1.active_field[1] == 8:  # pole 8,8
-            player1.active_field[1] -= result
-        elif player1.active_field[1] in range(1, 8) and player1.active_field[0] == 8:  # dolny wiersz
-            player1.active_field[1] -= result
-            if player1.active_field[1] < 0 and player1.active_field[0] == 8:
-                keep = player1.active_field[1] * -1
-                player1.active_field[1] = 0
-                player1.active_field[0] = 8
-                player1.active_field[0] -= keep
-        elif player1.active_field[0] == 8 and player1.active_field[1] == 0:  # pole 8,0
-            player1.active_field[0] -= result
-        elif player1.active_field[0] in range(1, 8) and player1.active_field[1] == 0:
-            player1.active_field[0] -= result
-            if player1.active_field[0] < 0 and player1.active_field[1] == 0:
-                keep = player1.active_field[0] * -1
-                player1.active_field[0] = 0
-                player1.active_field[1] = 0
-                player1.active_field[1] += keep
-        chance = random.randint(1, 100)
-        if chance <= 20:
-            player1.is_attacked = 1
-        card = None
-        active_player = active_player * -1
+        player1.move(torchAmount)
     elif player == '2':
-        player2.is_attacked = 0
-        player2.has_drawn = 0
-        result = random.randint(1, 6)
-        if player2.active_field[1] == 0 and player2.active_field[0] == 0:  # pole 0,0
-            player2.active_field[1] += result
-        elif player2.active_field[1] in range(1, 8) and player2.active_field[0] == 0:  # górny wiersz
-            player2.active_field[1] += result
-            if player2.active_field[1] > 8 and player2.active_field[0] == 0:
-                keep = player2.active_field[1] - 8
-                player2.active_field[1] = 8
-                player2.active_field[0] += keep
-        elif player2.active_field[0] == 0 and player2.active_field[1] == 8:  # pole 0,8
-            player2.active_field[0] += result
-        elif player2.active_field[0] in range(1, 8) and player2.active_field[1] == 8:  # prawa kolumna
-            player2.active_field[0] += result
-            if player2.active_field[0] > 8 and player2.active_field[1] == 8:
-                keep = player2.active_field[0] - 8
-                player2.active_field[0] = 8
-                player2.active_field[1] -= keep
-        elif player2.active_field[0] == 8 and player2.active_field[1] == 8:  # pole 8,8
-            player2.active_field[1] -= result
-        elif player2.active_field[1] in range(1, 8) and player2.active_field[0] == 8:  # dolny wiersz
-            player2.active_field[1] -= result
-            if player2.active_field[1] < 0 and player2.active_field[0] == 8:
-                keep = player2.active_field[1] * -1
-                player2.active_field[1] = 0
-                player2.active_field[0] = 8
-                player2.active_field[0] -= keep
-        elif player2.active_field[0] == 8 and player2.active_field[1] == 0:  # pole 8,0
-            player2.active_field[0] -= result
-        elif player2.active_field[0] in range(1, 8) and player2.active_field[1] == 0:
-            player2.active_field[0] -= result
-            if player2.active_field[0] < 0 and player2.active_field[1] == 0:
-                keep = player2.active_field[0] * -1
-                player2.active_field[0] = 0
-                player2.active_field[1] = 0
-                player2.active_field[1] += keep
-        chance = random.randint(1, 100)
-        if chance <= 20:
-            success_chance = random.randint(1, 50) + player2.strength + player2.resistance
-            if success_chance <= 25:
-                player2.health -= 1
-                if player2.health <= 0:
-                    player2.active_field = [8, 8]
-                    player2.health = 3
-                    player2.has_torch = 0
-                    for item in player2.keys:
-                        if player2.keys[item] == 1:
-                            player2.keys[item] = 0
-                            break
-            elif 25 <= success_chance <= 40:
-                player2.strength += 1
-            else:
-                player2.resistance += 1
-        card = None
-        active_player = active_player * -1
+        player2.move(torchAmount)
     return redirect(url_for('solo'))
 
 
 @app.route('/battle')
 def battle():
-    attack_power = random.randint(1, 6) + player1.strength
-    enemy_attack = random.randint(1, 6) - player1.resistance
-    if attack_power > enemy_attack:
-        check = random.randint(1, 2)
-        if check == 1:
-            player1.resistance += 1
-        else:
-            player1.strength += 1
-    else:
-        player1.health -= 1
-    player1.is_attacked = 0
-    if player1.health <= 0:
-        player1.active_field = [0, 0]
-        player1.health = 3
-        player1.has_torch = 0
-        for item in player1.keys:
-            if player1.keys[item] == 1:
-                player1.keys[item] = 0
-                break
-
+    player1.battle()
     return redirect(url_for('solo'))
 
 
 @app.route('/battleBoss/<bossID>')
-def battleBoss(bossID=None):
-    attack_power = random.randint(1, 6) + player1.strength
-    boss_attack = random.randint(1, 10) - player1.resistance
-    if attack_power > boss_attack:
-        if bossID == 'death':
-            player1.souls['death'] = 1
-        elif bossID == 'bone':
-            player1.souls['bone'] = 1
-        elif bossID == 'toxic':
-            player1.souls['toxic'] = 1
-        elif bossID == 'madness':
-            player1.souls['madness'] = 1
-    else:
-        player1.health -= 1
-        if player1.health <= 0:
-            player1.active_field = [0, 0]
-            player1.health = 3
-            for item in player1.keys:
-                if player1.keys[item] == 1:
-                    player1.keys[item] = 0
-                    break
-            player1.has_torch = 0
+def battleBoss(bossID):
+    player1.battleBoss(bossID)
     return redirect(url_for('solo'))
 
 
 @app.route('/draw')
 def draw():
-    player1.has_drawn = 1
-    global Deck
-    random.shuffle(Deck)
-    global card
-    if Deck:
-        card = Deck.pop()
-    else:
-        Deck = cards.build_deck()
-        card = Deck.pop()
-    if card.name == 'loseHealth':
-        player1.health -= 1
-        if player1.health <= 0:
-            player1.active_field = [0, 0]
-            player1.health = 3
-            player1.has_torch = 0
-            for item in player1.keys:
-                if player1.keys[item] == 1:
-                    player1.keys[item] = 0
-                    break
-    elif card.name == 'KeyBone':
-        player1.keys["bone"] = 1
-    elif card.name == 'KeyDeath':
-        player1.keys["death"] = 1
-    elif card.name == 'KeyToxic':
-        player1.keys["toxic"] = 1
-    elif card.name == 'KeyMadness':
-        player1.keys["madness"] = 1
-    elif card.name == 'magicArrow':
-        player2.health -= 1
-        if player2.health <= 0:
-            player2.active_field = [8, 8]
-            player2.health = 3
-            player2.has_torch = 0
-            for item in player2.keys:
-                if player2.keys[item] == 1:
-                    player2.keys[item] = 0
-                    break
-    elif card.name == 'recoverHealth':
-        player1.health += 1
-    elif card.name == 'thief':
-        for item in player2.keys:
-            if player2.keys[item] == 1:
-                player2.keys[item] = 0
-                break
-    elif card.name == 'loseItem':
-        for item in player1.keys:
-            if player1.keys[item] == 1:
-                player1.keys[item] = 0
-                break
-    elif card.name == "torch":
-        player1.has_torch = 1
+    player1.drawCard(player2)
     return redirect(url_for('solo'))
 
 
 @app.route('/solo/')
 def solo():
     return render_template('game_solo.html', player1=player1, player2=player2, Deck=Deck,
-                           keep=keep, result=result, ConstantDeck=ConstantDeck, card=card, active_player=active_player)
+                           keep=keep, ConstantDeck=ConstantDeck, card=card, active_player=active_player)
 
 
 @app.route('/loadPlayer', methods=['GET', 'POST'])
